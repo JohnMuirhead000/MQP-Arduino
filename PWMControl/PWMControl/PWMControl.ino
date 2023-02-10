fa11dd2100409d642f4c2c2e81e83b35d46ec27e
Daniel Correa <dcorrea012@gmail.com>
2023-02-10 01:47:51 -0500

    Set up code ofr L2 communication with Blue robotics

--- PWMControl/PWMControl/PWMControl.ino
+++ PWMControl/PWMControl/PWMControl.ino
@@ -1,9 +1,48 @@
+// THERMISTOR SPECIFICATIONS
+// resistance at 25 degrees C
+#define THERMISTORNOMINAL 10000      
+// temp. for nominal resistance (almost always 25 C)
+#define TEMPERATURENOMINAL 25   
+// The beta coefficient of the thermistor (usually 3000-4000)
+#define BCOEFFICIENT 3900
+// the value of the 'other' resistor
+#define SERIESRESISTOR 3300
+
 void setup() {
   // put your setup code here, to run once:
+
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+}
+
+float voltage() {
+  return voltage_raw*0.0004921;
+}
+
+float current() {
+  (return current_raw-32767)*0.001122;
+}
+
+float temperature(_temp_raw) {
+  // This code was taken from an Adafruit
+  float resistance = SERIESRESISTOR/(65535/float(_temp_raw)-1);
+
+  float steinhart;
+  steinhart = resistance / THERMISTORNOMINAL;  // (R/Ro)
+  steinhart = log(steinhart);                  // ln(R/Ro)
+  steinhart /= BCOEFFICIENT;                   // 1/B * ln(R/Ro)
+  steinhart += 1.0 / (TEMPERATURENOMINAL + 273.15); // + (1/To)
+  steinhart = 1.0 / steinhart;                 // Invert
+  steinhart -= 273.15;                         // convert to C
+
+  return steinhart;
+}
+
+float rpm() {  
+  _rpm = float(_rpm)/((uint16_t(millis())-_rpmTimer)/1000.0f)*60/float(_poleCount);
+  _rpmTimer = millis();
 }
